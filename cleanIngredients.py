baae58f3878f57dac2d18644f1ba6fbc14e7c31e
dorothyle <46880658+dorothyle@users.noreply.github.com>
2024-04-09 01:11:33 -0700

    create dataset with NER applied to ingredients

--- cleanIngredients.py
+++ cleanIngredients.py
@@ -4,24 +4,31 @@ from getIngredients import *
 # open file to read
 original_file = open('Food Ingredients and Recipe Dataset with Image Name Mapping.csv')
 reader = csv.reader(original_file)
-# TODO: open file to write to
-# cleaned_file = open('CLEANED Dataset.csv', 'w')
-# writer = csv.writer(cleaned_file)
+# open file to write to
+cleaned_file = open('CLEANED Dataset.csv', 'w')
+writer = csv.writer(cleaned_file)
 
 # iterate through file to read
 i = 0
 for row in reader:
-    if i != 0:
+    if i == 0:
+        row.append("NER")
+    else:
         # extract uncleaned ingredients into list
         uncleaned_ingredients = row[5][2:-2].split("', '")
-        print("UNCLEANED:", uncleaned_ingredients)
+        # print("UNCLEANED:", uncleaned_ingredients)
 
         # receive cleaned ingredients with NER
         cleaned_ingredients = cleanIngredients(uncleaned_ingredients)
-        print("CLEANED:", cleaned_ingredients)
-        print()
-        # TODO: append cleaned ingredients to row
-    # TODO: add each row to new file
+        # print("CLEANED:", cleaned_ingredients)
+        # print()
+
+        # append cleaned ingredients to row
+        row.append(cleaned_ingredients)
+        # print("NEW ROW:", row)
+
+    # add each row to new file
+    writer.writerow(row)
     i += 1
-    if i == 8:
-        break
+    # if i == 8:
+    #     break
